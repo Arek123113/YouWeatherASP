cb930273ddbac3dd3ff1ac8c4de82f084940c667
Arek <arek123113@gmail.com>
2018-05-07 16:56:16 +0200

    just screenshot

---
+++ YouWeather/Services/IIPadress.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YouWeather.Services
+{
+    public interface IIPadress
+    {
+	    Task<string> GetUsersIpAdress();
+    }
+}
